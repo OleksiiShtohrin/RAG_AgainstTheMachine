3a507e852efb1b961d060e94e6ca7c9d7f0bf58a
Oleksii Shtohrin <oshtohri@c3r6s4.42urduliz.com>
2026-08-31 12:06:26 +0200

    Updade src/models/question.py

--- src/models/question.py
+++ src/models/question.py
@@ -1,14 +1,17 @@
 """Data models for unanswered and answered questions and datasets."""
 
+import uuid
 from typing import List, Union
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from src.models.source import MinimalSource
 
 
 class UnansweredQuestion(BaseModel):
     """Represents a query without an answer."""
 
-    question_id: str
+    question_id: str = Field(
+        default_factory=lambda: str(uuid.uuid4()),
+    )
     question: str
 
 
